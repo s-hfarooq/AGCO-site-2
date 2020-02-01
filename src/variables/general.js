4bb76efce39ce5c2fdd236c09de9b09f5097d175
Hassan Farooq <1hassanfarooq@gmail.com>
2020-02-01 12:53:02 -0600

    d

--- src/variables/general.js
+++ src/variables/general.js
@@ -11,9 +11,9 @@ var website = [
   "TunnelFan 7 - Room 274 has lower than normal current draw"
 ];
 var server = [
-  "Lines From Great Russian Literature? Or E-mails From My Boss?",
-  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
-  'Sign contract for "What are conference organizers afraid of?"'
+  "Brady E. replaced TunnelFan4 on JAN 28 2020",
+  "Brady E. replaced TunnelFan3 on JAN 28 2020",
+  "Grant A. replaced TunnelFan3 on NOV 12 2019"
 ];
 
 module.exports = {
