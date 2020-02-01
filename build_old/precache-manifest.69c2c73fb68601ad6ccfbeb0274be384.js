486baf6fdb3a8b5ec647ff648b83ec5c12074273
Hassan Farooq <1hassanfarooq@gmail.com>
2020-02-01 10:15:47 -0600

    f

---
+++ build_old/precache-manifest.69c2c73fb68601ad6ccfbeb0274be384.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5b02bd9233eede092f2a18d48e52d2",
+    "url": "/material-dashboard-react/index.html"
+  },
+  {
+    "revision": "7a1abe3e73809a2305cf",
+    "url": "/material-dashboard-react/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "c0fc2eae16a011d511fc",
+    "url": "/material-dashboard-react/static/css/main.0fa7ec49.chunk.css"
+  },
+  {
+    "revision": "7a1abe3e73809a2305cf",
+    "url": "/material-dashboard-react/static/js/2.c95716e9.chunk.js"
+  },
+  {
+    "revision": "c0fc2eae16a011d511fc",
+    "url": "/material-dashboard-react/static/js/main.85516230.chunk.js"
+  },
+  {
+    "revision": "b31b170c222092005215",
+    "url": "/material-dashboard-react/static/js/runtime~main.ce1216ca.js"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/material-dashboard-react/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "d437250f0d85b7f738e6886802b4fb68",
+    "url": "/material-dashboard-react/static/media/sidebar-2.d437250f.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/material-dashboard-react/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/material-dashboard-react/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "a58794908e84f5004250ab11c344ad7b",
+    "url": "/material-dashboard-react/static/media/work.a5879490.jpg"
+  }
+]);
---
+++ src/variables/general.js
@@ -0,0 +1,24 @@
+// ##############################
+// // // Tasks for TasksCard - see Dashboard view
+// #############################
+
+var bugs = [
+  "Replace TunnelFan 4 - Room 392",
+  "Replace TunnelFan 6 - Room 340",
+];
+var website = [
+  "TunnelFan 2 - Room 340 has higher than normal current draw",
+  "TunnelFan 7 - Room 274 has lower than normal current draw"
+];
+var server = [
+  "Lines From Great Russian Literature? Or E-mails From My Boss?",
+  "Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit",
+  'Sign contract for "What are conference organizers afraid of?"'
+];
+
+module.exports = {
+  // these 3 are used to create the tasks lists in TasksCard - Dashboard view
+  bugs,
+  website,
+  server
+};
